1fbddf469b8b6370582109a2ab3fa16ce5f8de0c
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-03-10 18:57:38 -0400

    basic file structure established and created hello world program

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import HomePage from "./pages/HomePage"
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
-
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </Router>
   )
 }
 
