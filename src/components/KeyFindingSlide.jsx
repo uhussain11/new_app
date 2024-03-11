873ed9d84df42d1e0c1d1d3569f60e52e6587d87
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-03-10 20:04:49 -0400

    created key findings slides
    NOTE: number colors change on hover!

---
+++ src/components/KeyFindingSlide.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useState} from 'react';
+
+const KeyFindingSlide = (props) => {
+
+  const [hovered, setHovered] = useState(false);
+  const color = `h-[95vh] border border-[#000] ml-4 mr-4 mt-4 mb-4 rounded-sm`;
+  return (
+    <div className={color} onMouseEnter={() => setHovered(true)}
+    onMouseLeave={() => setHovered(false)}>
+      <h2 className="text-2xl"> <span style={{color: hovered ? props.color : "black"}}> {props.number} </span> </h2>
+    </div>
+  );
+};
+
+export default KeyFindingSlide;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import KeyFindingSlide from '../components/KeyFindingSlide';
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+  const slides = [];
+  const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3', '#FF1493', '#FFD700'];
+
+  for (let i = 1; i <= 9; i++){
+    slides.push(
+      <div key ={i}>
+        <KeyFindingSlide number={i} color={colors[i-1]}/>
+      </div>
+    )
+  }
+  return ( 
+    <>{slides}</>
   );
 };
 
